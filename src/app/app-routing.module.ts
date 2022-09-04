9b730463c39e925b81ad54c58a92367e82522660
DonfackNoubissie <noubissiepavel@gmail.com>
2022-09-04 02:22:57 +0100

    Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MainComponent} from "./main/main.component";
 import {RouterModule, Routes} from "@angular/router";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {RegistrationComponent} from "./registration/registration.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes= [
   {path: 'main', component: MainComponent},
   {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path:'', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: '', redirectTo: '/registration', pathMatch: 'full'},
+
 ];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent,
+    RegistrationComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
